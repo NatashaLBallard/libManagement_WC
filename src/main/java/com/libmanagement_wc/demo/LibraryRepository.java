c2064b6c307381affb561b1f65ba07892071d380
natashalballard <nbitcareer@gmail.com>
2018-02-09 14:48:34 -0500

    Initial commit

---
+++ src/main/java/com/libmanagement_wc/demo/LibraryRepository.java
@@ -0,0 +1,6 @@
+package com.libmanagement_wc.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LibraryRepository extends CrudRepository<Library, Long>{
+}
---
+++ src/test/java/com/libmanagement_wc/demo/LibManagementWcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.libmanagement_wc.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibManagementWcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
